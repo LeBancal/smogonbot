9511625a9914c284cab454027abb588e3105fc3f
LeBancal <62794843+LeBancal@users.noreply.github.com>
2020-06-01 13:11:09 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+const smogon = new Discord.Client()
+const google = require ('./commands/google')
+
+smogon.on('ready', function() {
+    smogon.user.setAvatar('./smogon.png')
+    smogon.user.setActivity('Showdown')
+
+})
+
+smogon.on('message', function (message) {
+    if (google.match(message)) {
+        google.action(message)
+        }
+})
+
+smogon.login('process.env.TOKEN')
