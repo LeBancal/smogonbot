6cc411183d06f96a5526ffdfa46fb5664e993de4
LeBancal <62794843+LeBancal@users.noreply.github.com>
2020-06-02 10:54:22 +0200

    Update google.js

--- .gitignore/google.js
+++ .gitignore/google.js
@@ -11,6 +11,12 @@ module.exports = class Google {
            args.shift()
            message.reply('https://www.smogon.com/dex/ss/moves/' + args.join('%20'))
        }
+       if (message.content.startsWith('!talent')) {
+           let args = message.content.split(' ')
+           args.shift()
+           message.reply('https://www.smogon.com/dex/ss/abilities/' + args.join('%20'))
+       }
+
     }
 
 
