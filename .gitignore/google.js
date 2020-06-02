1847a9f8b2b7815471d2daeac860ab893ac6d1a4
LeBancal <62794843+LeBancal@users.noreply.github.com>
2020-06-02 10:39:15 +0200

    Update google.js

--- .gitignore/google.js
+++ .gitignore/google.js
@@ -1,13 +1,17 @@
 module.exports = class Google {
 
     static match (message) {
-        return message.content.startsWith('!smogon')
+       if (message.content.startsWith('!smogon')) {
+           let args = message.content.split (' ')
+           args.shift()
+           message.reply('https://www.smogon.com/dex/ss/pokemon/' + args.join('%20'))
+       }
+       if (message.content.startsWith('!atk')) {
+           let args = message.content.split (' ')
+           args.shift()
+           message.reply('https://www.smogon.com/dex/ss/moves/' + args.join('%20'))
+       }
     }
 
-    static action (message ) {
-        let args = message.content.split (' ')
-        args.shift()
-        message.reply('https://www.smogon.com/dex/ss/pokemon/' + args.join('%20'))
-    }
 
 }
